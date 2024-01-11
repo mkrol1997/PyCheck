d20626aeb61ef41adcefe2b4dca9c5cdd9ded050
mkrol1997 <golotka333@gmail.com>
2024-01-11 19:15:31 +0100

    Separated game classes

---
+++ checkers_app/game_engine/board.py
@@ -0,0 +1,15 @@
+from checkers_app.game_engine.piece import Piece
+
+
+class Board:
+    def __init__(self):
+        self.matrix = [
+            [Piece(0), Piece(-1), Piece(0), Piece(-1), Piece(0), Piece(-1), Piece(0), Piece(-1)],
+            [Piece(-1), Piece(0), Piece(-1), Piece(0), Piece(-1), Piece(0), Piece(-1), Piece(0)],
+            [Piece(0), Piece(-1), Piece(0), Piece(-1), Piece(0), Piece(-1), Piece(0), Piece(-1)],
+            [Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0)],
+            [Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0), Piece(0)],
+            [Piece(1), Piece(0), Piece(1), Piece(0), Piece(1), Piece(0), Piece(1), Piece(0)],
+            [Piece(0), Piece(1), Piece(0), Piece(1), Piece(0), Piece(1), Piece(0), Piece(1)],
+            [Piece(1), Piece(0), Piece(1), Piece(0), Piece(1), Piece(0), Piece(1), Piece(0)],
+        ]
