d2880852ea0e8e8f2376e3379b294c19c514a0eb
mkrol1997 <golotka333@gmail.com>
2024-01-11 19:29:28 +0100

    Added client socketio events

---
+++ checkers_app/checkers/static/js/socketio/game_events.js
@@ -0,0 +1,62 @@
+function getAvailableMoves() {
+    return new Promise((resolve, reject) => {
+        socket.emit("get_available_moves", {"channel": channel })
+
+        socket.on('available_moves', (response) => {
+            resolve(response.moves);
+        });
+    });
+}
+
+function getCordsAfterMove(pawn) {
+     return new Promise((resolve, reject) => {
+        let pawn_cords = pawn.getAttribute('data-position').split('-');
+        socket.emit("get_cords_after_move",  {'pawn': pawn_cords, "channel": channel })
+
+        socket.on('cords_after_move', (response) => {
+            resolve(response.square_cords);
+        });
+    });
+}
+
+function movePawn(from_cords, to_cords) {
+    socket.emit("make_move",  {'from_cords': from_cords, 'to_cords': to_cords, "channel": channel})
+}
+
+function updateBoard() {
+    socket.emit('get_matrix', { "channel": channel });
+    socket.on('receive_matrix', (data) => {
+        drawChessBoard(data.matrix);
+    });
+}
+
+function joinRoom() {
+    socket.emit('join_room', { "room_id": channel});
+}
+
+function playGame(){
+    getCurrentPlayerMoves();
+}
+
+socket.on('connect', () => {
+    socket.emit('user_connected', { "channel": channel });
+});
+
+socket.on('play_game', () => {
+    playGame();
+});
+
+socket.on('handle_player_dc', () => {
+    socket.emit('client_disconnect', { 'channel': channel });
+});
+
+socket.on("game_finished", (result) => {
+    document.getElementById('result_reason').innerText = result.status;
+
+    if (result.winner == '1') {
+        document.getElementById('winner_img').src = pawn_black_img;
+    } else {
+        document.getElementById('winner_img').src = pawn_white_img;
+    }
+    $('#modalContent').modal('show');
+});
