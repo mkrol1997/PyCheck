23512fdfdc88bb800b98816fcd1b8e4618ea123b
mkrol1997 <golotka333@gmail.com>
2024-01-11 19:42:03 +0100

    Modified run method params

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-from checkers import create_app, socketio
+from checkers_app.checkers import create_app, socketio
 
 if __name__ == "__main__":
     app = create_app()
-    socketio.run(app=app, host="127.0.0.1", port=8000)
+    socketio.run(app=app, host="0.0.0.0", port=8000, allow_unsafe_werkzeug=True)
